04d98bb9d5838c96034eb018ebe045f2b6ab45de
eyern <eyern254@gmail.com>
2026-01-30 23:06:23 +0300

    static

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -168,8 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/media")]
 
