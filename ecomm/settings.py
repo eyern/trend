8a4e91a28f62418eb1f36f64d197976773cfb9ae
eyern <eyern254@gmail.com>
2026-02-09 12:57:25 +0300

    manifest removal

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -174,7 +174,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/media")]
 
 #whitenoise static stuff
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
