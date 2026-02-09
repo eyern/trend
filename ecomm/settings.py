f4d892049795a92bc1619d39e1d4626051ac4cd9
eyern <eyern254@gmail.com>
2026-02-09 13:36:13 +0300

    static files

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -167,20 +167,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/media")]
-
-#whitenoise static stuff
+#Static Files Whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Media files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = "media/"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, '/app/data')
-MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
