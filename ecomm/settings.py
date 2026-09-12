5621156244f61dffbfe04fd8145c6613de80d5ad
eyern <eyern254@gmail.com>
2026-09-12 09:28:00 +0300

    statics

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -24,8 +24,6 @@ load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production
 DEBUG = False
